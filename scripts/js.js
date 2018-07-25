22d11442df7dc7364f34684f74115fb64d2c6e43
Matty <NeuroticDog@github.com>
2018-07-25 14:55:00 +0200

    ovni launching

---
+++ scripts/js.js
@@ -0,0 +1,22 @@
+let launch = document.querySelector(".launch")
+let countdown = document.querySelector(".countdown")
+let ovni = document.querySelector("#ovni")
+let number = 10
+
+launch.addEventListener(
+    'click',
+    function () {
+        setInterval(decollage, 1000);
+    }, false);
+
+function decollage() {
+    if (number > 0) {
+        countdown.innerHTML = (number -= 1)
+    } else {
+      countdown.innerHTML = 0
+        ovni.style.transform = "translateY(-15000px)"
+        ovni.style.transition = "all 15s"
+        interrogation.style.transform = "translateY(-15000px)"
+        interrogation.style.transition = "all 15s"
+    }
+}
