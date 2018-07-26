7a0cc36f9c3c5a0c15ae2f5e4af19898048f6088
Matty <NeuroticDog@github.com>
2018-07-26 11:26:51 +0200

    countdown centered

--- scripts/js.js
+++ scripts/js.js
@@ -9,24 +9,32 @@ launch.addEventListener(
         setInterval(decollage, 1000);
     }, false);
 
-function animateClock(span) {
-    span.className = "turn";
-    setTimeout(function () {
-        span.className = "";
+/*function animateClock(span) {
+    let i = true;
+    setInterval(function () {
+       if (i === true) {
+           span.classList.add("turn")
+           i = false;
+       }
+       if (i === false) {
+           span.classList.add("")
+           i = true;
+       }
     }, 700);
-}
+}*/
 
 function decollage() {
     countdown.innerHTML = '<span>' + number + '</span>';
 
-    var spans = countdown.getElementsByTagName("span");
-    animateClock(spans[0]);
+/*    var spans = document.querySelector(".countdown span");
+    animateClock(spans);*/
     if (number > 0) {
         countdown.innerHTML = (number -= 1)
-        animateClock(spans[0])
+//        animateClock(spans)
     } else {
       countdown.innerHTML = 0
         ovni.style.transform = "translateY(-15000px)"
         ovni.style.transition = "all 15s"
+//        countdown.style.textAlign = "center"
     }
 }
